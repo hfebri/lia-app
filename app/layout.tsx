b1c0a0dd7018217abaada6b52afd546e5510dcda
Helmi <hfebri@leverategroup.asia>
2025-11-20 16:05:05 +0700

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { AuthProvider } from "@/components/auth/auth-provider";
 import { ConversationsProvider } from "@/components/providers/conversations-provider";
@@ -46,6 +47,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F19JC8LYNW"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-F19JC8LYNW');
+          `}
+        </Script>
         <VersionMigrationProvider>
           <ErrorBoundary>
             <ThemeProvider
